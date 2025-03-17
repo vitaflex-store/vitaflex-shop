f93c83fb833d8c7fa42893ae6550c44492bf21e1
vitaflex-store <gmohnkern0309@gmail.com>
2025-03-17 19:31:14 +0100

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-      <h1>🏋️ Willkommen bei VitaFlex</h1>
-      <p>Dein Shop für Fitness-Zubehör und natürliche Energieprodukte 🌿</p>
-    </main>
-  );
-}
